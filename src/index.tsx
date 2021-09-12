00d2081c8a18e6008ef1c0bd4e181e85d306ded7
PeterHo83 <68497446+PeterHo83@users.noreply.github.com>
2021-09-12 13:07:49 +1200

    Implement UI for the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,41 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { createGlobalStyle } from 'styled-components'
+import React, { useEffect } from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/app-store';
+import { fetchImages } from './services/image-services';
+import { useViewport } from './utils/util-functions';
+import { createGlobalStyle } from 'styled-components';
 
 // Import main Board component
-import { Board } from './components/board'
+import Board from './components/board';
+// Import Header component
+import { Header } from './components/header';
 
 // Use createGlobalStyle to change the background of 'body' element
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #4bcffa;
+    height: 100vh;
+    margin: 0;
   }
-`
+`;
 
 // Create component for the page
-const Page = () => (<>
-  <Board />
-  <GlobalStyle />
-</>)
+const Page = () => {
+  const { width } = useViewport();
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Header />
+      <Board />
+      <GlobalStyle />
+    </Provider>
+  );
+};
 
 // Render the page into DOM
-ReactDOM.render(<Page />, document.getElementById('root'))
-
+ReactDOM.render(<Page />, document.getElementById('root'));
